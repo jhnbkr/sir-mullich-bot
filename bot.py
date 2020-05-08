fcbffa32628fbcad108d3efc864a2c3641fb92d0
John Baker <john@theyoungastronauts.com>
2020-05-07 21:27:32 -0400

    Added `flip`, `roll`, and `choice` commands

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 import os
 import random
-import numpy as np
 
+import numpy as np
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="sir ")
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.command()
+async def choose(ctx, *choices: str):
+    await ctx.send(random.choice(choices))
 
 
 @bot.command()
@@ -18,6 +23,23 @@ async def team(ctx, teams_count: int, *names: str):
     teams = list(np.array_split(names, teams_count))
     for c, t in enumerate(teams):
         await ctx.send(f"Team {c + 1}: {', '.join(t)}")
+
+
+@bot.command()
+async def flip(ctx):
+    await ctx.send("Heads" if bool(random.getrandbits(1)) else "Tails")
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split("d"))
+    except ValueError:
+        await ctx.send("Format has to be in NdN!")
+        return
+
+    result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
 
 bot.run(TOKEN)
